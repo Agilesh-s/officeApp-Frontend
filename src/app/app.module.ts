b43142c1dd5ab9a69db6e59982ac05df01ef09e9
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-28 12:23:48 +0530

    Admin Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-
+const appRoutes:Routes=[
+  {
+    path:"",component:AdminLoginComponent
+  },
+  {
+    path:"dashboard",component:AdminDashboardComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    HttpClientModule
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
